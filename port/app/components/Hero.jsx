6e6662fceb166462973c0010a3c9ddd4867d5b06
AlainDLC <jd_alain@hotmail.com>
2025-06-18 12:24:19 +0200

    hero sectiopn

--- port/app/components/Hero.jsx
+++ port/app/components/Hero.jsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section id="hero" className="m-0 p-0">
+    <section id="hero">
       <div className="bg-[url('/a.png')] bg-cover bg-center bg-no-repeat h-32 flex flex-col lg:h-[45vh]  items-center    ">
         <Image
           src="/jag.jpg"
           alt="Alain Profilbild"
           width={150}
           height={150}
-          className="rounded-full h-20 w-20  md:h-50 md:w-50  shadow-2xl   "
+          className="rounded-full h-10 w-10  md:h-40 md:w-40  mt-18 mr-55 xl: lg:mt-80 lg:mr-[1200px] md:ml-[150px]  md:mt-36  "
         />
       </div>
     </section>
